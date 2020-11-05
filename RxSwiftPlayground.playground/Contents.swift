b1d87afd762d55d36cff2c84cc712f3ec73985b1
Lucas <lucaspedrazoli@gmail.com>
2020-11-05 18:17:24 -0300

    add session challenge

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -152,3 +152,65 @@ example(of: "Black jack challenge") {
 
   deal(3)
 }
+
+example(of: "Session challenge") {
+
+  enum UserSession {
+    case loggedIn, loggedOut
+  }
+
+  enum LoginError: Error {
+    case invalidCredentials
+  }
+
+  let disposeBag = DisposeBag()
+
+  // Create userSession BehaviorRelay of type UserSession with initial value of .loggedOut
+  let userSession = BehaviorRelay(value: UserSession.loggedOut)
+
+  // Subscribe to receive next events from userSession
+  userSession
+    .subscribe(
+    onNext: { print("User session update: ", $0 )})
+    .disposed(by: disposeBag)
+
+  func logInWith(username: String, password: String, completion: (Error?) -> Void) {
+    guard username == "johnny@appleseed.com",
+          password == "appleseed" else {
+            completion(LoginError.invalidCredentials)
+      return
+    }
+
+    // Update userSession
+    userSession.accept(.loggedIn)
+  }
+
+  func logOut() {
+    // Update userSession
+    userSession.accept(.loggedOut)
+  }
+
+  func performActionRequiringLoggedInUser(_ action: () -> Void) {
+    // Ensure that userSession is loggedIn and then execute action()
+    if userSession.value == .loggedIn {
+      action()
+    }
+  }
+
+  for i in 1...2 {
+    let password = i % 2 == 0 ? "appleseed" : "password"
+
+    logInWith(username: "johnny@appleseed.com", password: password) { error in
+      guard error == nil else {
+        print(error!)
+        return
+      }
+
+      print("User logged in.")
+    }
+
+    performActionRequiringLoggedInUser {
+      print("Successfully did something only a logged in user can do.")
+    }
+  }
+}
